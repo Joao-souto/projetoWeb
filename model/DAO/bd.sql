180d90d2f57d7c6bc876fc4b9eacede1b45634df
João Souto <joaosouto0@outlook.com>
2024-11-13 08:37:53 -0300

    update estrutura inicial

---
+++ model/DAO/bd.sql
@@ -0,0 +1,17 @@
+CREATE TABLE usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE publicacoes (
+    id_publicacao INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT,
+    descricao VARCHAR(255) NOT NULL,
+    anexo VARCHAR(255),
+    data_publicacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('ativo', 'excluido', 'pendente') DEFAULT 'ativo',
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE
+);
