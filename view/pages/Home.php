26381c8b1ad859c90ff07ff813c41a19f57e99c2
João Souto <joaosouto0@outlook.com>
2024-11-30 00:05:46 -0300

    Home update

--- view/pages/Home.php
+++ view/pages/Home.php
@@ -14,24 +14,57 @@
         <img src="../IMG/vsco2.png" alt="Logo da página" id="vsco2">
 
         <a href="" class="linkMenu">
-                <div class="divLinkMenu">
-                    <img src="../IMG/feedIcon.png" alt="Icon do feed">
-                    <h3>FEED</h3>
-                </div>
+            <div class="divLinkMenu">
+                <img src="../IMG/feedIcon.png" alt="Icon do feed">
+                <h3>FEED</h3>
+            </div>
         </a>
 
         <a href="" class="linkMenu">
-                <div class="divLinkMenu">
-                    <img src="../IMG/profileIcon.png" alt="Icon do feed">
-                    <h3>PROFILES</h3>
-                </div>
+            <div class="divLinkMenu">
+                <img src="../IMG/profileIcon.png" alt="Icon do feed">
+                <h3>PROFILES</h3>
+            </div>
         </a>
-
-        </div>
-        </div>
+        <a href="" class="botao">EXIT</a>        
     </header>
+    <button id="postBotao">NEW POST</button>
     <main>
-        <h1>AQUI FICARÁ O FEED</h1>
+        <h1>VSCO — For creators, by creators</h1>
+
+        <section class="photo-gallery">
+
+            <div class="photo-item">
+                <img src="../IMG/ft1.jpg" alt="Descrição da foto 1">
+                <p class="photo-caption">Descrição da Foto 1</p>
+            </div>
+
+            <div class="photo-item">
+                <img src="../IMG/ft2.jpg" alt="Descrição da foto 1">
+                <p class="photo-caption">Descrição da Foto 1</p>
+            </div>
+
+            <div class="photo-item">
+                <img src="../IMG/f3.jpg" alt="Descrição da foto 1">
+                <p class="photo-caption">Descrição da Foto 1</p>
+            </div>
+
+            <div class="photo-item">
+                <img src="../IMG/ft4.jpg" alt="Descrição da foto 1">
+                <p class="photo-caption">Descrição da Foto 1</p>
+            </div>
+
+            <div class="photo-item">
+                <img src="../IMG/ft5.jpg" alt="Descrição da foto 1">
+                <p class="photo-caption">Descrição da Foto 1</p>
+            </div>
+
+            <div class="photo-item">
+                <img src="../IMG/ft6.jpg" alt="Descrição da foto 1">
+                <p class="photo-caption">Descrição da Foto 1</p>
+            </div>
+            </div>
+        </section>
     </main>
 </body>
 
