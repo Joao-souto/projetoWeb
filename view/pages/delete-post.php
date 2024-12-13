1f20d676168c9401ee4118bc5089f0685ebe2d54
João Souto <joaosouto0@outlook.com>
2024-12-13 01:25:01 -0300

    update em delete cliente

--- view/pages/delete-post.php
+++ view/pages/delete-post.php
@@ -3,9 +3,28 @@ include("../../util/Protect.php");
 require_once '/xampp/htdocs/projetoWeb/controller/PublicacoesController.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
-    $idPublicacao = intval($_GET['id']);
 
-    if (PublicacoesController::deletarPublicacao($idPublicacao)) {
+    function apagarImagem(){
+        $idPublicacao = intval($_GET['id']);
+        $publicacao = PublicacoesController::consultarPublicacao($idPublicacao);
+        $imagem = $publicacao['anexo'];
+
+        // Verifica se o arquivo existe
+        if (file_exists($imagem)) {
+            // Tenta deletar o arquivo
+            if (unlink($imagem)) {
+                PublicacoesController::deletarPublicacao($idPublicacao);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            echo "A imagem não existe.";
+        } 
+    }
+
+
+    if (apagarImagem()) {
         // Redireciona para a home com a mensagem de sucesso
         header("Location: home.php?success=1");
     } else {
